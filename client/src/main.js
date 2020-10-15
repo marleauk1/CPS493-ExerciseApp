29c3f9325b48585172d4b705c8fdb2888aa093eb
warblr <marleauk@newpaltz.edu>
2020-10-16 00:47:54 +0900

    added buefy, fontawesome

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'buefy/dist/buefy.css'
+import '@fortawesome/fontawesome-free/css/ass.css'
+
 Vue.config.productionTip = false
 
 new Vue({
