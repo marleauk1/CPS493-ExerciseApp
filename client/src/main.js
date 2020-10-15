f6da7e081d447585b53f4e97b9ad9ffbfb971471
warblr <marleauk@newpaltz.edu>
2020-10-16 01:23:27 +0900

    re-did vue cli

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-import 'buefy/dist/buefy.css'
-import '@fortawesome/fontawesome-free/css/all.css'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
